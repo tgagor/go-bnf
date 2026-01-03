8a17c36c11df2cc6fd4cf4281973b23d3f9bccea
Tomasz Gągor <tomasz@gagor.pl>
2026-01-03 17:16:25 +0100

    feat: add grammar validation and output of AST tree to the CLI tool

--- cmd/cli.go
+++ cmd/cli.go
@@ -13,18 +13,24 @@ type CLI struct {
 	BuildVersion string
 	AppName      string
 
-	GrammarFile string
-	VerifyFile  string
-	LineByLine  bool
+	GrammarFile     string
+	VerifyFile      string
+	LineByLine      bool
+	ValidateGrammar bool
+	ParseAsAST      bool
+	StartRule       string
 }
 
-func New(buildVersion, appName, grammarFile, verifyFile string, lineByLine bool) *CLI {
+func New(buildVersion, appName, grammarFile, verifyFile string, lineByLine, validateGrammar, parseAsAST bool, startRule string) *CLI {
 	return &CLI{
-		BuildVersion: buildVersion,
-		AppName:      appName,
-		GrammarFile:  grammarFile,
-		VerifyFile:   verifyFile,
-		LineByLine:   lineByLine,
+		BuildVersion:    buildVersion,
+		AppName:         appName,
+		GrammarFile:     grammarFile,
+		VerifyFile:      verifyFile,
+		LineByLine:      lineByLine,
+		ValidateGrammar: validateGrammar,
+		ParseAsAST:      parseAsAST,
+		StartRule:       startRule,
 	}
 }
 
@@ -64,7 +70,22 @@ func (cli *CLI) Run() error {
 		fmt.Println("Parsing error:", err)
 		os.Exit(1)
 	}
-	fmt.Println("Grammar loaded.")
+
+	if cli.StartRule != "" {
+		g.SetStart(cli.StartRule)
+	}
+
+	// Always validate grammar structure (undefined rules, etc.)
+	if err := g.ValidateGrammar(); err != nil {
+		fmt.Printf("Grammar validation error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Grammar loaded and validated.")
+
+	if cli.ValidateGrammar {
+		// If only validation was requested, we are done
+		return nil
+	}
 
 	if cli.VerifyFile == "" {
 		stat, err := os.Stdin.Stat()
@@ -91,23 +112,38 @@ func (cli *CLI) Run() error {
 
 	for _, l := range tokens {
 		fmt.Printf("Checking: %s", l)
-		match, err := g.Match(l)
-		if match {
-			fmt.Println(" -> matched")
-		} else {
+		if cli.ParseAsAST {
+			node, err := g.Parse(l)
 			if err == nil {
-				fmt.Println(" -> not matched (no error details)")
-			} else if pe, ok := err.(*bnf.ParseError); ok {
-				if pe == nil {
-					fmt.Println(" -> not matched (nil ParseError)")
-				} else {
-					fmt.Printf("\n%s\n\n", pe.Pretty(l))
-				}
+				fmt.Println(" -> matched")
+				fmt.Println("AST Tree:")
+				fmt.Println(node.String())
 			} else {
-				fmt.Printf(" -> error: %v\n", err)
+				cli.reportError(l, err)
+			}
+		} else {
+			match, err := g.Match(l)
+			if match {
+				fmt.Println(" -> matched")
+			} else {
+				cli.reportError(l, err)
 			}
 		}
 	}
 
 	return nil
+}
+
+func (cli *CLI) reportError(input string, err error) {
+	if err == nil {
+		fmt.Println(" -> not matched (no error details)")
+	} else if pe, ok := err.(*bnf.ParseError); ok {
+		if pe == nil {
+			fmt.Println(" -> not matched (nil ParseError)")
+		} else {
+			fmt.Printf("\n%s\n\n", pe.Pretty(input))
+		}
+	} else {
+		fmt.Printf(" -> error: %v\n", err)
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"go-bnf/cmd"
-	"fmt"
-	"os"
 	"flag"
+	"fmt"
+	"go-bnf/cmd"
+	"os"
 )
 
 var BuildVersion string // Will be set dynamically at build time.
@@ -15,6 +15,9 @@ var inputFile string
 var help bool
 var version bool
 var lineByLine bool
+var validateGrammar bool
+var parseAsAST bool
+var startRule string
 
 func main() {
 
@@ -22,13 +25,16 @@ func main() {
 	flag.StringVar(&grammarFile, "g", "", "Path to the BNF grammar file")
 	flag.StringVar(&inputFile, "i", "", "Path to the input file to verify against the grammar")
 	flag.BoolVar(&lineByLine, "l", false, "Verify input line by line otherwise as a whole")
+	flag.BoolVar(&validateGrammar, "v", false, "Only validate the grammar file")
+	flag.BoolVar(&parseAsAST, "p", false, "Parse input and show AST")
+	flag.StringVar(&startRule, "s", "", "Override the start rule for the grammar")
 	flag.BoolVar(&help, "h", false, "Show help")
-	flag.BoolVar(&version, "v", false, "Show version")
+	flag.BoolVar(&version, "version", false, "Show version")
 	flag.Parse()
 
 	if help {
-		fmt.Println("Usage: bnf -g <grammar-file> -i <input-file>")
-		fmt.Println("   or: cat <input-file> | bnf -g <grammar-file>")
+		fmt.Println("Usage: bnf -g <grammar-file> -i <input-file> [options]")
+		fmt.Println("   or: cat <input-file> | bnf -g <grammar-file> [options]")
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
@@ -39,12 +45,12 @@ func main() {
 	}
 
 	if grammarFile == "" {
-		fmt.Println("Usage: bnf -g <grammar-file> -i <input-file>")
-		fmt.Println("   or: cat <input-file> | bnf -g <grammar-file>")
+		fmt.Println("Usage: bnf -g <grammar-file> -i <input-file> [options]")
+		fmt.Println("   or: cat <input-file> | bnf -g <grammar-file> [options]")
 		os.Exit(1)
 	}
 
-	cli := cmd.New(BuildVersion, appName, grammarFile, inputFile, lineByLine)
+	cli := cmd.New(BuildVersion, appName, grammarFile, inputFile, lineByLine, validateGrammar, parseAsAST, startRule)
 	if err := cli.Run(); err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
